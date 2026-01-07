a5ab89c9d053a0fc7ef061dc831502e8c2b9f80c
Phan Thi Linh Tra <phan.thi.linh.tra@petrik.hu>
2026-01-07 10:29:27 +0100

    gravitáció

--- FlappyBird/MainWindow.xaml.cs
+++ FlappyBird/MainWindow.xaml.cs
@@ -5,9 +5,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FlappyBird
 {
@@ -16,9 +18,21 @@ namespace FlappyBird
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer gameTimer = new DispatcherTimer();
         public MainWindow()
         {
             InitializeComponent();
-		}
+
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            gameTimer.Tick += GameLoop;
+            gameTimer.Start();
+        }
+
+        double gravity = 2;
+        public void GameLoop(object sender, EventArgs e)
+        {
+            double y = Canvas.GetTop(bird);
+            Canvas.SetTop(bird, y + gravity);
+        }
 	}
 }
