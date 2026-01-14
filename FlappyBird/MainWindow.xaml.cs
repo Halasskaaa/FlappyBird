afac575167075fe4ba93117d8bb9e2f326dd1130
Phan Thi Linh Tra <phan.thi.linh.tra@petrik.hu>
2026-01-14 09:04:45 +0100

    Akadályok (Pipes/Csövek) hozzáadása

--- FlappyBird/MainWindow.xaml.cs
+++ FlappyBird/MainWindow.xaml.cs
@@ -21,7 +21,11 @@ namespace FlappyBird
         DispatcherTimer gameTimer = new DispatcherTimer();
 
 		double gravity = 2;
-		double jumpStrength = -15;
+		double jumpStrength = -12;
+
+        List<Rectangle> pipes = new List<Rectangle>();
+        Random rnd = new Random();
+        int pipeTimer = 0;
 
 		public MainWindow()
         {
@@ -36,19 +40,100 @@ namespace FlappyBird
 
         public void GameLoop(object sender, EventArgs e)
         {
-            double y = Canvas.GetTop(bird);
+
+			double y = Canvas.GetTop(bird);
             Canvas.SetTop(bird, y + gravity);
-
             gravity += 1;
-        }
 
-        public void OneKeyDown(object sender, KeyEventArgs e)
+			if (y < 0 || y + bird.Height > gameCanvas.ActualHeight)
+			{
+				GameOver();
+			}
+
+
+			pipeTimer++;
+			if (pipeTimer > 90)
+			{
+				MakePipePair();
+				pipeTimer = 0;
+			}
+
+			for (int i = pipes.Count - 1; i >= 0; i--)
+			{
+				double x = Canvas.GetLeft(pipes[i]);
+				if (x + pipes[i].Width < 0)
+				{
+					gameCanvas.Children.Remove(pipes[i]);
+					pipes.RemoveAt(i);
+				}
+				else
+				{
+					Canvas.SetLeft(pipes[i], x - 5);
+				}
+			}
+
+			foreach (var pipe in pipes)
+			{
+				Rect pipeHitBox = new Rect(Canvas.GetLeft(pipe), Canvas.GetTop(pipe), pipe.Width, pipe.Height);
+
+				double birdX = Canvas.GetLeft(bird);
+				double birdY = Canvas.GetTop(bird);
+				Rect birdHitBox = new Rect(birdX, birdY, bird.Width, bird.Height);
+
+				if (birdHitBox.IntersectsWith(pipeHitBox))
+				{
+					GameOver();
+				}
+			
+			}
+		}
+
+		public void OneKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Space)
             {
                 gravity = jumpStrength;
             }
         }
+
+        Rectangle CreatePipe(double x, double y, double width, double height)
+        {
+            Rectangle pipe = new Rectangle
+            {
+                Width = width,
+                Height = height,
+                Fill = Brushes.Green
+            };
+            Canvas.SetLeft(pipe, x);
+            Canvas.SetTop(pipe, y);
+            gameCanvas.Children.Add(pipe);
+            pipes.Add(pipe);
+            return pipe;
+		}
+
+		public void MakePipePair()
+		{
+			double gapHeight = 150;
+			double pipeWidth = 60;
+			double minPipeHeight = 50;
+
+			double canvasHeight = gameCanvas.ActualHeight;
+			double canvasWidth = gameCanvas.ActualWidth;
+
+			double maxPipeHeight = canvasHeight - gapHeight - minPipeHeight;
+
+			double topPipeHeight = rnd.Next((int)minPipeHeight, (int)maxPipeHeight);
+			double bottomPipeHeight = canvasHeight - topPipeHeight - gapHeight;
+
+			CreatePipe(canvasWidth, 0, pipeWidth, topPipeHeight);
+			CreatePipe(canvasWidth, canvasHeight - bottomPipeHeight, pipeWidth, bottomPipeHeight);
+		}
+
+		public void GameOver()
+		{
+			gameTimer.Stop();
+			MessageBox.Show("Game Over!");
+		}
 	}
 }
 
